2362ebf63461b5bd7e3ef6a3f54845515663b4fd
kubadguy <kubadguy@users.noreply.github.com>
2025-12-29 13:45:10 +0000

    deploy: e417b827ce5767b7f5ec3c468c71f977f959557b

--- _app/immutable/chunks/D5jcRxC7.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_q2i4xk?.base??"/social-project-stuff",t=globalThis.__sveltekit_q2i4xk?.assets??s??"";export{t as a,s as b};
---
+++ _app/immutable/chunks/DqVJXuTp.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_uh0wf1?.base??"/social-project-stuff",t=globalThis.__sveltekit_uh0wf1?.assets??s??"";export{t as a,s as b};
